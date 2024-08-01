9b3bec0b66fb5703d2ded598b45dc353f32a97ab
Arabella Echavez <90621776+arughh@users.noreply.github.com>
2024-08-01 18:28:59 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// Add any JavaScript you need for interactivity
+// For example, you can add a simple alert on page load
+window.addEventListener('load', function() {
+    console.log('Page is fully loaded');
+});
